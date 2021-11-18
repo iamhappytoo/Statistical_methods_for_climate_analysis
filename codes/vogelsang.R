783a1c17b9730ce5ce7cf44f4ce4bde27e7108cd
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2021-11-18 11:37:04 -0800

    Update vogelsang.R

--- codes/vogelsang.R
+++ codes/vogelsang.R
@@ -10,9 +10,6 @@
 #   if prewhite == 1 then prewhitening using an AR(1) model is used
 #   if prewhite == 0 no prewhitening is used.
 
-#   8/13/96
-
-
 sig2np <- function(v, M, my.kernel, prewhite){
   
   #    v = uhat;
